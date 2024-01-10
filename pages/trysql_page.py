75c1a4e5a32aea5f2d4c7b3828134350079d7109
Sergei_Kamendrovskii <se25051990@gmail.com>
2024-01-10 21:18:36 +0400

    break through the endless page loading

--- pages/trysql_page.py
+++ pages/trysql_page.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import time
 
 
 class TrySqlPage:
@@ -12,11 +13,21 @@ class TrySqlPage:
         self.driver = driver
 
     def open_page(self):
-        print("loading the page...")
         self.driver.get("https://www.w3schools.com/sql/trysql.asp?filename=trysql_select_all")
+        try:
+            self.driver.get("https://www.w3schools.com/sql/trysql.asp?filename=trysql_select_all")
+        except TimeoutError:
+            pass
+
         # url = "https://www.w3schools.com/sql/trysql.asp?filename=trysql_select_all"
         # self.driver.execute_script(f"window.location.href = '{url}';")
-        print("the page is loaded")
+        # self.driver.execute_script("window.stop();")
+
+        # editor_element = self.driver.find_element(By.ID,
+        #                                      "textareaCodeSQL")
+        # text_to_insert = "SELECT * FROM Customers;"
+        # self.driver.execute_script(f"arguments[0].innerText = '{text_to_insert}';", editor_element)
+        # self.driver.switch_to.default_content()
 
     def click_run_sql(self):
         self.driver.find_element(*self.RUN_SQL_BUTTON).click()
@@ -24,8 +35,9 @@ class TrySqlPage:
         # element = wait.until(EC.element_to_be_clickable(*self.RUN_SQL_BUTTON))
         #
         # element.click()
-        WebDriverWait(self.driver, 3).until(
-            EC.visibility_of_element_located(*self.NUMBER_OF_RECORDS_RETURNED))
+        # WebDriverWait(self.driver, 3).until(
+        #     EC.visibility_of_element_located(*self.NUMBER_OF_RECORDS_RETURNED))
+        time.sleep(5)
 
     # def check_if_request_executed(self):
 
--- tests/conftest.py
+++ tests/conftest.py
@@ -7,11 +7,16 @@ from webdriver_manager.chrome import ChromeDriverManager
 @pytest.fixture
 def driver():
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument('--headless')
+    # chrome_options.add_argument('--headless')
+    chrome_options.page_load_strategy = 'eager'
+    chrome_options.add_argument('--allow-insecure-localhost')
     chrome_options.add_argument('--ignore-certificate-errors')
     chrome_options.add_argument("--ignore-ssl-errors")
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
     chrome_options.add_argument('--log-level=3')
 
+    # chrome_driver_path = "driver/chromedriver_win.exe"
     chrome_driver_path = ChromeDriverManager().install()
     service = Service(executable_path=chrome_driver_path)
 
