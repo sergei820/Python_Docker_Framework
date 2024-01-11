c5de060df0c9211d65ed2b46710cdc7bb1246816
Sergei_Kamendrovskii <se25051990@gmail.com>
2024-01-11 21:59:29 +0400

    scenario 1 implementation is in progress

--- pages/trysql_page.py
+++ pages/trysql_page.py
@@ -28,7 +28,6 @@ class TrySqlPage:
         number_of_records_element = WebDriverWait(self.driver, 3).until(
             ec.visibility_of_element_located(self.NUMBER_OF_RECORDS_RETURNED))
         number_of_records_found = int(number_of_records_element.text.replace("Number of Records:", ''))
-        print("I'm here")
         print(number_of_records_found)
 
         # element = self.driver.find_element(*self.NUMBER_OF_RECORDS_RETURNED)
@@ -36,11 +35,43 @@ class TrySqlPage:
         # print(text_of_element)
         self.driver.switch_to.default_content()
 
-    def check_address_for_contact_name(self, address: str, contact_name: str):
-        """ContactName               'Giovanni Rovelli'        имеет
-        Address = 'Via Ludovico il Moro 22'.        """
+    def check_address_for_contactname(self, expected_address: str, contact_name: str):
+        customer_id = TrySqlPage.get_customer_id_by_text(self.driver, contact_name)
+        actual_address = TrySqlPage.get_field_value_by_customer_id(self.driver, 'Address', customer_id)
+        assert expected_address in actual_address
 
-    def get_customer_id_by_text(self, text_to_find):
-        customer_id_element = self.driver.find_element(By.XPATH, f"//td[contains(text(), '{text_to_find}')]/parent::tr/td[1]")
-        return int(customer_id_element.text)
-# //table[@class='w3-table-all notranslate']//tr/td[1]
+    @staticmethod
+    def get_customer_id_by_text(driver, text_to_find) -> int:
+        customer_id_selector = f"//td[contains(text(), '{text_to_find}')]/parent::tr/td[1]"
+        print(f"SELECTOR: {customer_id_selector}")
+        customer_id_element = driver.find_element(By.XPATH, customer_id_selector)
+
+        # while not customer_id_element.is_displayed():
+        #     # Scroll down using JavaScript
+        #     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+        customer_id = int(customer_id_element.text)
+        return customer_id
+
+    @staticmethod
+    def get_field_value_by_customer_id(driver, field_name: str, customer_id: int) -> str:
+        if field_name == 'CustomerName':
+            column_index = 2
+        elif field_name == 'ContactName':
+            column_index = 3
+        elif field_name == 'Address':
+            column_index = 4
+        elif field_name == 'City':
+            column_index = 5
+        elif field_name == 'PostalCode':
+            column_index = 6
+        elif field_name == 'Country':
+            column_index = 7
+        else:
+            raise TypeError("Please, choose the correct column name")
+        searched_element = driver.find_element(By.XPATH, f"//table[@class='w3-table-all notranslate']"
+                                                              f"//tr[{str(customer_id + 2)}]/td[{column_index}]")
+        field_value = searched_element.text
+        return field_value
+
+
