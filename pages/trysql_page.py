3fd8df50d1ef71e9726619ea019fa129455c3d1f
Sergei_Kamendrovskii <se25051990@gmail.com>
2024-01-12 11:42:00 +0400

    scenario 2 - in progress

--- pages/trysql_page.py
+++ pages/trysql_page.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
+from utils.utils import Utils
 
 
 class TrySqlPage:
@@ -31,41 +32,13 @@ class TrySqlPage:
         assert number_of_records_found > 0
 
     def check_address_for_contact_name(self, expected_address: str, contact_name: str):
-        customer_id = TrySqlPage.get_customer_id_by_text(self.driver, contact_name)
-        actual_address = TrySqlPage.get_field_value_by_customer_id(self.driver, 'Address', customer_id)
+        customer_id = Utils.get_customer_id_by_text(self.driver, contact_name)
+        actual_address = Utils.get_field_value_by_customer_id(self.driver, 'Address', customer_id)
         print(f"expected_address : {expected_address}")
         print(f"actual_address : {actual_address}")
         assert expected_address in actual_address
 
     def edit_select_request(self, reuqest_specification: str):
         pass
-
-    @staticmethod
-    def get_customer_id_by_text(driver, text_to_find) -> int:
-        customer_id_selector = f"//td[contains(text(), '{text_to_find}')]/parent::tr/td[1]"
-        customer_id_element = driver.find_element(By.XPATH, customer_id_selector)
-        customer_id = int(customer_id_element.text)
-        return customer_id
-
-    @staticmethod
-    def get_field_value_by_customer_id(driver, field_name: str, customer_id: int) -> str:
-        if field_name == 'CustomerName':
-            column_index = 2
-        elif field_name == 'ContactName':
-            column_index = 3
-        elif field_name == 'Address':
-            column_index = 4
-        elif field_name == 'City':
-            column_index = 5
-        elif field_name == 'PostalCode':
-            column_index = 6
-        elif field_name == 'Country':
-            column_index = 7
-        else:
-            raise TypeError("Please, choose the correct column name")
-        searched_element = driver.find_element(By.XPATH, f"//table[@class='w3-table-all notranslate']"
-                                                              f"//tr[{str(customer_id + 1)}]/td[{column_index}]")
-        field_value = searched_element.text
-        return field_value
 
 
---
+++ utils/utils.py
@@ -0,0 +1,31 @@
+from selenium.webdriver.common.by import By
+
+
+class Utils:
+    @staticmethod
+    def get_customer_id_by_text(driver, text_to_find) -> int:
+        customer_id_selector = f"//td[contains(text(), '{text_to_find}')]/parent::tr/td[1]"
+        customer_id_element = driver.find_element(By.XPATH, customer_id_selector)
+        customer_id = int(customer_id_element.text)
+        return customer_id
+
+    @staticmethod
+    def get_field_value_by_customer_id(driver, field_name: str, customer_id: int) -> str:
+        if field_name == 'CustomerName':
+            column_index = 2
+        elif field_name == 'ContactName':
+            column_index = 3
+        elif field_name == 'Address':
+            column_index = 4
+        elif field_name == 'City':
+            column_index = 5
+        elif field_name == 'PostalCode':
+            column_index = 6
+        elif field_name == 'Country':
+            column_index = 7
+        else:
+            raise TypeError("Please, choose the correct column name")
+        searched_element = driver.find_element(By.XPATH, f"//table[@class='w3-table-all notranslate']"
+                                                              f"//tr[{str(customer_id + 1)}]/td[{column_index}]")
+        field_value = searched_element.text
+        return field_value
