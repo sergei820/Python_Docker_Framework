305ce31b2d40d0250175ee306ff6a94ff2d3d0f5
Sergei_Kamendrovskii <se25051990@gmail.com>
2024-01-12 13:34:52 +0400

    scenario 4 - in progress

--- tests/update_test.py
+++ tests/update_test.py
@@ -1,5 +1,8 @@
 from pages.trysql_page import TrySqlPage
 
 
-def test_address_for_contactname(driver):
+def test_update_operation(driver):
     """4. Обновить все поля (кроме CustomerID) в любой записи таблицы Customers и проверить, что изменения записались в базу."""
+    'UPDATE Customers SET "CustomerID" = 4, "CustomerName" = "NewName", "ContactName" = "NewContactName", "Address" = "NewAddr", "City" = "NewCity", "PostalCode" = "NewPostalCode", "Country" = "NewCountry" WHERE "CustomerID" = 4;'
+    pass
+
